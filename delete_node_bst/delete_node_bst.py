bc72c16991793adbe10b606d137c5ce60ca07deb
Tetiana Shvets <shvets.pn@ucu.edu.ua>
2024-04-22 16:34:50 +0300

    add delete node bst

---
+++ delete_node_bst/delete_node_bst.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if root is None:
+            return root
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            if root.right is None:
+                return root.left
+            cur=root.right
+            while cur.left is not None:
+                cur = cur.left
+            min_node = cur
+            root.val = min_node.val
+            root.right = self.deleteNode(root.right, min_node.val)
+        return root
